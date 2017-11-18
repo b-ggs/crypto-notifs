7ad93adb05884eb69552c58d09ac281f811b2003
boggs <hello@boggs.xyz>
2017-11-19 03:19:15 +0800

    Clean up Coins.ph API

--- lib/api/coins_ph.rb
+++ lib/api/coins_ph.rb
@@ -1,11 +1,12 @@
 class Api::CoinsPh < Api::Base
   def initialize(options = {})
     @exchange = 'Coins.ph'
-    @url = "https://quote.coins.ph/v1/markets/#{options[:product]}-#{options[:currency]}"
+    product = options[:product]
+    currency = options[:currency]
+    @url = "https://quote.coins.ph/v1/markets/#{product}-#{currency}"
   end
 
   def parse_response
-    @market = response['market']['symbol']
     @product = response['market']['product']
     @currency = response['market']['currency']
     @buy = response['market']['ask']

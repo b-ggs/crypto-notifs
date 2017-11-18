24d714a2ffb331fece6b2f1c4eadd445fe25ff08
boggs <hello@boggs.xyz>
2017-11-19 03:34:10 +0800

    Update test driver

--- main.rb
+++ main.rb
@@ -8,18 +8,25 @@ options = {
   currency: 'PHP',
 }
 coins_ph_data = Api::CoinsPh.new(options).run
+coinbase_data = Api::Coinbase.new(options).run
+
+# options = {
+#   data: coins_ph_data,
+#   attribute: 'buy',
+#   comparison_operator: '<',
+#   threshold_value: '330000',
+#   skip_condition: true,
+# }
+# Notifier::Telegram.new(options).run
 
 options = {
   data: coins_ph_data,
-  attribute: 'buy',
-  comparison_operator: '<',
-  threshold_value: '330000',
   skip_condition: true,
 }
-Notifier::Telegram.new(options).run
+Notifier::Stdout.new(options).run
 
 options = {
-  data: coins_ph_data,
+  data: coinbase_data,
   skip_condition: true,
 }
 Notifier::Stdout.new(options).run

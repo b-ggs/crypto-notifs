e8538d20f7f52c4ec36cf7cce4e215b3bf42aa9e
boggs <hello@boggs.xyz>
2018-02-09 00:32:11 +0800

    Refactor - Remove Notifier related classes

--- main.rb
+++ main.rb
@@ -10,28 +10,9 @@ options = {
 require 'lib/api/coins_ph'
 coins_ph_data = Api::CoinsPh.new(options).run
 
+puts coins_ph_data
+
 require 'lib/api/coinbase'
 coinbase_data = Api::Coinbase.new(options).run
 
-# options = {
-#   data: coins_ph_data,
-#   attribute: 'buy',
-#   comparison_operator: '<',
-#   threshold_value: '330000',
-#   skip_condition: true,
-# }
-# Notifier::Telegram.new(options).run
-
-require 'lib/notifier/stdout'
-
-options = {
-  data: coins_ph_data,
-  skip_condition: true,
-}
-Notifier::Stdout.new(options).run
-
-options = {
-  data: coinbase_data,
-  skip_condition: true,
-}
-Notifier::Stdout.new(options).run
+puts coinbase_data

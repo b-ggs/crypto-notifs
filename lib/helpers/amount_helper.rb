3a2ef9e178f5cf31300ad5ef3400628361941b22
boggs <hello@boggs.xyz>
2018-02-09 02:10:21 +0800

    Fix pretty decimal formatting

--- lib/helpers/amount_helper.rb
+++ lib/helpers/amount_helper.rb
@@ -3,12 +3,16 @@ module AmountHelper
     sprintf_param =  "%.#{decimal_places}f"
     # https://stackoverflow.com/a/10541870
     # https://stackoverflow.com/a/11466770
-    amount =
-      sprintf(sprintf_param, number).
+    full_amount = sprintf(sprintf_param, amount).split('.')
+    whole_number = full_amount[0]
+    decimal = full_amount[1]
+    whole_number =
+      whole_number.
       to_s.
       reverse.
       gsub(/(\d{3})(?=\d)/, '\\1,').
       reverse
+    "#{whole_number}.#{decimal}"
   end
 
   def pretty_fiat(amount, currency, options = {})
